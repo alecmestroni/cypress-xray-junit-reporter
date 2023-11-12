903330a14ff49bffa6660d5660bb55a59170c016
Alec <alecmestroni@gmail.com>
2023-11-12 23:13:33 +0100

    minor fixes to sample test

--- test/cypress/e2e/sampleTest.cy.js
+++ test/cypress/e2e/sampleTest.cy.js
@@ -1,40 +1,40 @@
-describe('Suite 0', { testIsolation: false }, () => {
-	describe('Suite 1', { testIsolation: false }, () => {
-		it('testCase 1.1', { jiraKey: 'pippo1.1', retries: 1 }, () => {
+describe('testSuite 0', () => {
+	describe('testSuite 1', () => {
+		it('testCase 1.1', { jiraKey: 'test1.1', retries: 1 }, () => {
+			expect(true).to.eq(false);
+		})
+	})
+	describe('testSuite 2', () => {
+		it('testCase 2.1', { missingJiraKey: 'here' }, () => {
+			expect(true).to.eq(true);
+		})
+		it('testCase 2.2', () => {
+			expect(true).to.eq(true);
+		})
+		it('testCase 2.3', () => {
 			expect(true).to.eq(true);
 		})
 	})
-	describe('Suite 2', { testIsolation: false }, () => {
-		it('testCase 2.1', { retries: 1 }, () => {
+	describe('testSuite 3', () => {
+		it('testCase 3.1', { jiraKey: 'test3.1' }, () => {
 			expect(true).to.eq(true);
 		})
-		it('testCase 2.2', { retries: 1 }, () => {
-			expect(true).to.eq(true);
-		})
-		it('testCase 2.3', { retries: 1 }, () => {
+		it('testCase 3.2', { jiraKey: 'test3.2' }, () => {
 			expect(true).to.eq(true);
 		})
 	})
-	describe('Suite 3', { testIsolation: false }, () => {
-		it('testCase 3.1', { jiraKey: 'pippo1.1', retries: 1 }, () => {
-			expect(true).to.eq(true);
-		})
-		it('testCase 3.1', { jiraKey: 'pippo1.1', retries: 1 }, () => {
-			expect(true).to.eq(true);
-		})
-	})
-	describe('Suite 4', { testIsolation: false }, () => {
-		it('testCase 4.1', { jiraKey: 'pippo1.1', retries: 1 }, () => {
+	describe('testSuite 4', () => {
+		it('testCase 4.1', { jiraKey: 'test4.1.1' }, () => {
 			expect(true).to.eq(true);
 		})
 	})
 
-	describe('Suite 4', { testIsolation: false }, () => {
-		it('testCase 4.1', { jiraKey: 'pippo1.2', retries: 1 }, () => {
+	describe('testSuite 4', () => {
+		it('testCase 4.1', { jiraKey: 'test4.1.2' }, () => {
 			expect(true).to.eq(true);
 		})
 	})
-	it('testCase 0.1', { jiraKey: 'pippo1.2', retries: 1 }, () => {
+	it('testCase 0.1', { jiraKey: 'test0.1' }, () => {
 		expect(true).to.eq(true);
 	})
 })
